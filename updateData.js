4af264bda4419d9d94d7c8be59f54bf37a0a1180
Sefi Cohen <67965039+seficohen1@users.noreply.github.com>
2022-09-08 17:08:10 +0200

    adding update commands of books and authors in insertData file

---
+++ updateData.js
@@ -0,0 +1,20 @@
+// the following commands are used to update DB data //
+
+
+
+db.authors.updateOne(
+  {name: 'George' , lastName: 'Orwell'} , /* Where to update */
+  {$set: {                                /* What to update */
+    dateOfDeath: new Date('1984')
+  }}
+)
+
+db.books.updateOne(
+  {title: 'The Color Purple'}, /* Where to update */
+  {$set: {releaseYear: new Date('2000-01-01')}} /* What to update */
+)
+
+db.books.updateOne(
+  {title: 'Pride and Prejudice'}, 
+  {$set: {title : 'Pride and Prejudice - New Edition'}}
+)
